fd8023ab7ba0766543e306f6b062281f9dabc71f
ashwani1122 <ashwanisingh3846@gmail.com>
2026-03-29 19:24:30 +0530

    added url

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   const next = searchParams.get("next") ?? "/";
-
+  console.log({next , code})
   if (code) {
     const supabase = await createClient();
     const { error, data } = await supabase.auth.exchangeCodeForSession(code);
@@ -16,15 +16,16 @@ export async function GET(request: Request) {
 
       const forwardedHost = request.headers.get("x-forwarded-host");
       const isLocalEnv = process.env.NODE_ENV === "development";
-
+      console.log(isLocalEnv);
       if (isLocalEnv) {
         return NextResponse.redirect(`${origin}${next}`);
       }
 
       if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        console.log(`the forwarder url${forwardedHost}`);
+        return NextResponse.redirect(`https://claude-sub-agent.vercel.app/${next}`);
       }
-
+      console.log(`the origin is : ${origin}`);
       return NextResponse.redirect(`${origin}${next}`);
     }
 
